e4e445cb2484ba1676dc7992b7b0d5db457d43ee
Rajathaaa <rajatha.r.prasad@gmail.com>
2026-04-14 01:22:55 +0530

    adding jenkinsfile

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,10 @@
-package com.example;
+/*package com.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
@@ -20,4 +17,45 @@ public class App
         driver.findElement(By.id("login-button")).click();
     }
 }
+*/
+
+
+
+
+
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Set the path to geckodriver
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+        
+        // Step 2: Create a temporary Firefox profile (using a clean profile each time)
+        FirefoxOptions options = new FirefoxOptions();
+        options.setProfile(new FirefoxProfile());  // This creates a new, temporary profile
+        
+        // Initialize FirefoxDriver
+        WebDriver driver = new FirefoxDriver(options);
+
+        //WebDriver driver=new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+}
+
+
+
 
